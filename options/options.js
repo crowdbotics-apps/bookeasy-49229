6e06a29e601d5263ca7c98c11ddbf181f87ec105
Crowdbotics <team@crowdbotics.com>
2024-08-31 18:05:44 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bookeasy-49229",
+  url: "https://bookeasy-49229.botics.co",
+  api: "https://bookeasy-49229.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
